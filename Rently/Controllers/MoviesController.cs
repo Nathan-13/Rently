46f281b7b2fa9e7e663085eae6a1fb0b0d4ce83c
Ram <RamFattahRF@Gmail.com>
2020-03-19 21:13:59 -0700

    Setting Up Movies Model

---
+++ Rently/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rently.Models;
+
+namespace Rently.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek"};
+            return View(movie);
+        }
+    }
+}
