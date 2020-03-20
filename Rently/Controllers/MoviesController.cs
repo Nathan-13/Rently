df673b03fa9458b9d412f149dcf539fe54db6318
Ram <RamFattahRF@Gmail.com>
2020-03-19 21:24:05 -0700

    Customers model

--- Rently/Controllers/MoviesController.cs
+++ Rently/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Rently.Models;
+using Rently.ViewModel;
 
 namespace Rently.Controllers
 {
@@ -13,7 +14,20 @@ namespace Rently.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek"};
-            return View(movie);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
